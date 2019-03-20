89c57b96854efc78b350dd0e2cb60d6feacdb857
Yeewon <39365737+Yeewon@users.noreply.github.com>
2019-03-20 20:35:02 +0900

    django source code

---
+++ yw_source/mysite/elections/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=10)
+    introduction = models.TextField()
+    area = models.CharField(max_length=15)
+    party_number = models.IntegerField(default=0)
