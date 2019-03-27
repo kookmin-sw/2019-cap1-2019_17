cb943d66fa8aebc35acc954b660c0829fed07407
KMUKKS <kimgisuo@naver.com>
2019-03-27 17:24:08 +0900

    Rank 알고리즘 구현 및 텍스트기사 크롤링 후 요약 샘플 구현

--- sourceCode/skeleton.py
+++ sourceCode/skeleton.py
@@ -10,7 +10,7 @@ import numpy as np
 class SentenceTokenizer(object): # 문장형태소로 토큰화
     def __init__(self):
         self.kkma = Kkma()
-        self.twitter = Okt()
+        self.twitter = Twitter()
         textfile = open("한국어_불용어_리스트.txt", "r")
         self.stopwords = []
         while True:
@@ -90,6 +90,63 @@ class Rank(object): # Rank 알고리즘 적용
         return {idx: r[0] for idx, r in enumerate(ranks)}
 
 class TextRank(object):# TF-IDF값을 이어 받아 TextRank알고리즘 적용
+    def __init__(self, text):
+        # 분리한 문장들을 토큰화하고 sent_tokenize에 저장.
+        self.sent_tokenize = SentenceTokenizer()
+        
+        if text[:5] in ('http:', 'https'):
+            self.sentences = self.sent_tokenize.url2sentences(text)
+        else:
+            self.sentences = self.sent_tokenize.text2sentences(text)
+        
+        self.nouns = self.sent_tokenize.get_nouns(self.sentences)
+                    
+        self.graph_matrix = GraphMatrix()
+        
+        self.sent_graph = self.graph_matrix.build_sent_graph(self.nouns) # 문장 가중치 그래프
+        self.words_graph, self.idx2word = self.graph_matrix.build_words_graph(self.nouns) # 단어 가중치 그래프
+        
+        self.rank = Rank()
+        self.sent_rank_idx = self.rank.get_ranks(self.sent_graph)
+        self.sorted_sent_rank_idx = sorted(self.sent_rank_idx, key=lambda k: self.sent_rank_idx[k], reverse=True)
+        
+        self.word_rank_idx =  self.rank.get_ranks(self.words_graph)
+        self.sorted_word_rank_idx = sorted(self.word_rank_idx, key=lambda k: self.word_rank_idx[k], reverse=True)
+        
+        
+    def summarize(self, sent_num=3):
+        summary = []
+        index=[]
+        for idx in self.sorted_sent_rank_idx[:sent_num]:
+            index.append(idx)
+        
+        index.sort()
+        for idx in index:
+            summary.append(self.sentences[idx])
+        
+        return summary
+        
+    def keywords(self, word_num=10):
+        rank = Rank()
+        rank_idx = rank.get_ranks(self.words_graph)
+        sorted_rank_idx = sorted(rank_idx, key=lambda k: rank_idx[k], reverse=True)
+        
+        keywords = []
+        index=[]
+        for idx in sorted_rank_idx[:word_num]:
+            index.append(idx)
+            
+        #index.sort()
+        for idx in index:
+            keywords.append(self.idx2word[idx])
+        
+        return keywords
 
-#print('keywords :',textrank.keywords())
+url = 'http://v.media.daum.net/v/20170611192209012?rcmd=r'
+textrank = TextRank(url)
+#textrank = TextRank(a)
 
+for row in textrank.summarize(3):
+    print(row)
+    print()
+print('keywords :',textrank.keywords())
