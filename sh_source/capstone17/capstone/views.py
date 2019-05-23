00c86e1c03b0f57c0732e78f866afb56e0aceed0
양성호 <ysh8272002@naver.com>
2019-05-24 04:27:45 +0900

    upload bug fixed
    sh error fixed

--- sh_source/capstone17/capstone/views.py
+++ sh_source/capstone17/capstone/views.py
@@ -2,84 +2,44 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 from datetime import datetime
-
-#from .forms import SummaryForm
- 
 import os
 import sys
-#class OverwriteStorage(FileSystemStorage):
-#
-#    def get_available_name(self, name):
-#        if self.exists(name):
-#            os.remove(os.path.join(SOME_PATH, name))
-#        return name
-#fs_name = {}
-#time = {}
 
-
-#class OverwriteStorage(FileSystemStorage):
-#    def _save(self, name, content):
-#        if self.exists(name):
-#            self.delete(name)
-#        return super(OverwriteStorage, self)._save(name, content)
-#    def get_available_name(self, name):
-#        return name
 input_name = {}
+cnt = 1
 def index(request):
-	cnt = '1'
+	global cnt
+	html_name = {}
+	# file upload
 	if request.method == 'POST':
 		uploaded_file = request.FILES['file']
 		fs = FileSystemStorage()
 		name = fs.save(uploaded_file.name, uploaded_file)
 		input_name['name'] = name
+		html_name['name'] = name
+		cnt=2
 		print(input_name['name'])
-		
+	# url upload	
 	if 'url_input' in request.GET:
 		input_name['name'] = request.GET['url_input']
-		cnt = '2'
+		html_name['name'] = request.GET['url_input']
+		cnt=3
 		print(input_name['name'])
 
-#			sentence = request.GET['sentence']
-#			print(file_name)
-#			print(sentence)
-# file로 넣을 때와 url 입력 구별			
-#		os.system("sh ../mk.sh" + " " + name)
-		#+ " " + sentence
-#		context['url'] = uploaded_file.name
-#		os.system("sh ../run.sh")
-	
-	if 'sentence' in request.GET:
+	if 'sentence' in request.GET and request.GET['sentence']:
 		sentence = request.GET['sentence']
 		print(sentence)
-		if cnt == '1':
+		if cnt == 2:
 			print(input_name['name'])
-			#		os.system("sh ../flac.sh " + input_name['url'] + " " + sentence )
-		else:
+			print('CNT_TWO')
+			#		os.system("sh ../flac.sh " + input_name['name'] + " " + sentence )
+			cnt = 1
+			
+		if cnt == 3:
 			print(input_name['name'])
-		#		os.system("sh ../url.sh " + input_name['url'] + " " + sentence )
+			print('CNT_THREE')
+		#		os.system("sh ../url.sh " + input_name['name'] + " " + sentence )
+			cnt = 1
 		
-	return render(request, 'capstone/index.html', input_name)
-#	return render(request, 'capstone/index.html', context)
- 
-#def summary(request):
-#	if request.method == 'POST':
-#		
-#	return render(request, 'capstone/index.html')
-
-#def list(request):
-#	form = SummaryForm()
-#	return render(request, 'capstone/list.html', {
-#		'form': form
-#	})
-
-#def summary(request):
-#	
-#
-
-
-#def summary_list(request):
-#	return render(request, 'capstone/summary_list.html')
-#
-#def upload_summary(request):
-#	return render(request, 'capstone/upload_summary.html')
+	return render(request, 'capstone/index.html', html_name)
 
