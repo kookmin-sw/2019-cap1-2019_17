8bf265255652e2f3badb34938ea291e66cb783eb
okyungjin <okyungjin@gmail.com>
2019-05-25 21:08:19 +0900

    음성파일의 길이를 txt 파일로 저장

--- kyungj_source/getDuration.py
+++ kyungj_source/getDuration.py
@@ -1,12 +1,14 @@
-# get the length of audio file in python
+# 음성파일의 길이를 구하기
 
 # Ubuntu install 
 # $ sudo apt install sox
 
 # $ python3 getDuation.py test.flac
+
 import subprocess
 import sys
 
+# 음성파일의 길이를 구해서 test_duration.txt에 쓰기
 fileName = sys.argv[1] # file name
 duration = str(subprocess.check_output('sox --info -d ' + fileName, shell=True))
 duration = duration[2:]
@@ -14,8 +16,3 @@ duration = duration[:-6]
 
 with open(fileName[:-5] + '_duration.txt', 'w') as f:
     f.write(duration)
-
-
-
-
-
