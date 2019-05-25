3d888617b4282cc35a8e683f99b8b2dbf2b844f0
yeewon <yes3427@kookmin.ac.kr>
2019-05-25 19:18:06 +0900

    modify web mainpage

---
+++ yw_source/mysite/capstone_web/capstone/forms.py
@@ -0,0 +1,8 @@
+#from django import forms
+#from .models import Summary
+#
+#class SummaryForm(forms.ModelForm):
+#	class Meta:
+#		model = Summary
+#		fields = ('title', 'duration', 'Uploaded',  'KeyWords', 'text_summary', 'text_overall')
+#			
