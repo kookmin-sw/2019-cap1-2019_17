e6a4f81f374fedcaa8aff0eeaea5327e2009ffd7
okyungjin <okyungjin@gmail.com>
2019-05-25 21:10:48 +0900

    음성파일 업로드 시간 DB에 저장하는 코드 추가

--- kyungj_source/dataToDB.py
+++ kyungj_source/dataToDB.py
@@ -1,4 +1,4 @@
-# Mongo DB에 음성파일의 이름, keyword 5개, duration 업로드
+# Mongo DB에 음성파일의 이름, keyword 3개, duration 업로드
 # 음성파일: test.flac 
 # 필요한 파일: test_keyword.txt & test_duration.txt 
 
@@ -12,6 +12,7 @@ import pymongo
 import sys
 import os.path
 import subprocess
+import time
 
 # test_keyword.txt 파일이 있을 경우 실행
 try:
@@ -24,6 +25,7 @@ except:
 connection = pymongo.MongoClient()
 db = connection.test
 collection = db.test_collection
+
 post = {"filename" : sys.argv[1]}
 collection.insert_one(post)
 
@@ -46,3 +48,8 @@ with open(sys.argv[1] + '_duration.txt') as f:
 
 newvalues = { "$set" : {"duration" : duration}}
 collection.update_one(post, newvalues)
+
+# 현재 시간을 Mongo DB에 추가
+uploadTime = time.strftime('%d %b %Y %H:%M ', time.localtime(time.time()))
+newvalues = { "$set" : {"upload_time" : uploadTime[:-1]}}
+test_collection.update_one(post, newvalues)
