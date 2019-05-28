c0d1c6fb809e9aa67ac3a33d32aca5cf557cd353
yeewon <yes3427@kookmin.ac.kr>
2019-05-28 10:45:38 +0900

    delete unused files

--- yw_source/mysite/capst_web/custom.js
+++
@@ -1,8 +0,0 @@
-
-window.onload = function(){
-    var hw = document.getElementById('hw');
-    hw.reaFileSync('sample.txt','utf8');
-    hw.addEventListener('click', function(){
-        console.log(text);
-    })
-}
--- yw_source/mysite/capstone_web/capstone/forms.py
+++
@@ -1,8 +0,0 @@
-#from django import forms
-#from .models import Summary
-#
-#class SummaryForm(forms.ModelForm):
-#	class Meta:
-#		model = Summary
-#		fields = ('title', 'duration', 'Uploaded',  'KeyWords', 'text_summary', 'text_overall')
-#			
